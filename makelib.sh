c7c6bbca585f7213922afc89aba686faf9f50484
Alvaro <alvieboy@alvie.com>
2011-10-06 17:45:20 +0100

    serpro: fix arduino naming

--- makelib.sh
+++ makelib.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+set -e
 
-LIBFILES_1="SerProArduino.h crc16.cpp"
+LIBFILES_1="crc16.cpp"
 
 LIBFILES_2="crc16.h Packet.h SerProCommon.h SerProHDLC.h  preprocessor_table.h SerProArduino.h  SerPro.h SerProPacket.h"
 
@@ -9,17 +10,21 @@ tmpdir=`mktemp -d`;
 CWD=`pwd`;
 [ -z ${CWD} ] && exit -1
 
-mkdir -p ${tmpdir}/SerPro/serpro || exit -1
+mkdir -p ${tmpdir}/SerPro3/serpro || exit -1
 
 for file in ${LIBFILES_1}; do 
-	install -m 644 -D $file ${tmpdir}/SerPro || exit -1
+	install -m 644 -D $file ${tmpdir}/SerPro3 || exit -1
 done;
 
+install -m 644 SerProArduino.h ${tmpdir}/SerPro3/SerPro3.h  || exit
+
 for file in ${LIBFILES_2}; do 
-	install -m 644 -D $file ${tmpdir}/SerPro/serpro/ || exit -1
+	install -m 644 -D $file ${tmpdir}/SerPro3/serpro/ || exit -1
 done; 
 
-cd ${tmpdir} && tar cfvz ${CWD}/SerPro.tar.gz SerPro;
+cd ${tmpdir} && tar cfz ${CWD}/SerPro3.tar.gz SerPro3;
 cd ${CWD};
 
 rm -rf ${tmpdir}
+
+echo "Created SerPro3.tar.gz"
